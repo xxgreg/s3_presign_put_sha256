e37b0d07ed43797351609c878f6617b5234c8077
Greg Lowe <greg@montoux.com>
2024-04-19 09:39:46 +1200

    Shuffle a few things around

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"log"
 	"net/http"
 	"net/http/httputil"
+	"os"
 	"time"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
@@ -22,8 +23,8 @@ func main() {
 
 	ctx := context.Background()
 
-	bucket := "<<<<<the-bucket-name-here>>>>"
-	key := "<<<<<the-object-key-here>>>>"
+	bucket := os.Getenv("S3_BUCKET")
+	key := "798798"
 
 	body := []byte(`
 And both that morning equally lay
@@ -40,7 +41,16 @@ I doubted if I should ever come back.
 
 	s3c := s3.NewFromConfig(cfg)
 
-	req, err := PresignPutWithSha256(ctx, s3c, bucket, key, body)
+	checksumBytes := sha256.Sum256(body)
+	checksum := base64.StdEncoding.EncodeToString(checksumBytes[:])
+
+	req, err := PresignPutWithChecksum(ctx, s3c, PutArgs{
+		Bucket:         bucket,
+		Key:            key,
+		ChecksumSha256: checksum,
+		ContentLength:  int64(len(body)),
+		Expiry:         time.Hour,
+	})
 	if err != nil {
 		log.Fatalln(err)
 	}
@@ -52,8 +62,7 @@ I doubted if I should ever come back.
 	r.Header = req.SignedHeader
 
 	client := &http.Client{
-		// Uncomment to see all of the things.
-		// Transport: &loggingTransport{},
+		Transport: &loggingTransport{},
 	}
 	resp, err := client.Do(r)
 	if err != nil {
@@ -63,43 +72,59 @@ I doubted if I should ever come back.
 	fmt.Println("status", resp.Status)
 }
 
-func PresignPutWithSha256(
+type PutArgs struct {
+	Bucket         string
+	Key            string
+	ChecksumSha256 string
+	ContentLength  int64
+	ContentType    string
+	Expiry         time.Duration
+}
+
+func PresignPutWithChecksum(
 	ctx context.Context,
 	s3c *s3.Client,
-	bucket, key string,
-	body []byte,
+	args PutArgs,
 ) (*v4.PresignedHTTPRequest, error) {
 
-	checksumBytes := sha256.Sum256(body)
-	checksum := base64.StdEncoding.EncodeToString(checksumBytes[:])
+	contentType := args.ContentType
+	if contentType == "" {
+		contentType = "application/octet-stream"
+	}
 
+	if args.Expiry == 0 {
+		args.Expiry = time.Hour
+	}
+
+	// Workaround for: https://github.com/aws/aws-sdk/issues/480
 	withPresigner := func(opt *s3.PresignOptions) {
 		opt.Presigner = v4.NewSigner(func(so *v4.SignerOptions) {
-			// I copied the Default settings.
+			// These are the Default settings usually set in s3.newDefaultV4Signer()
 			o := s3c.Options()
 			so.Logger = o.Logger
 			so.LogSigning = o.ClientLogMode.IsSigning()
 			so.DisableURIPathEscaping = true
 
 			// This is the magic sauce which makes SHA256 checksums work.
+			// It causes the X-Amz-Sdk-Checksum-Algorithm, and X-Amz-Checksum-Sha256 to be included
+			// as http headers instead of query parameters in the url. The S3 backend currently
+			// silently ignores these if they are sent as query parameters.
 			so.DisableHeaderHoisting = true
 		})
 	}
 
-	psc := s3.NewPresignClient(s3c)
+	psc := s3.NewPresignClient(s3c, withPresigner)
 
 	in := &s3.PutObjectInput{
-		Bucket:            aws.String(bucket),
-		Key:               aws.String(key),
-		Body:              bytes.NewReader(body),
+		Bucket:            aws.String(args.Bucket),
+		Key:               aws.String(args.Key),
 		ChecksumAlgorithm: types.ChecksumAlgorithmSha256,
-		ChecksumSHA256:    aws.String(checksum),
-		// Try it with an incorrect checksum to make sure it fails.
-		//ChecksumSHA256: aws.String("bad+nDnO0vTnour8G+5YPPx4tLMvxgS0K3ZBusogZxU="),
-		ContentLength: aws.Int64(int64(len(body))),
+		ContentLength:     aws.Int64(args.ContentLength),
+		ChecksumSHA256:    aws.String(args.ChecksumSha256),
+		ContentType:       aws.String(args.ContentType),
 	}
 
-	return psc.PresignPutObject(ctx, in, s3.WithPresignExpires(time.Hour), withPresigner)
+	return psc.PresignPutObject(ctx, in, s3.WithPresignExpires(args.Expiry))
 }
 
 type loggingTransport struct{}
